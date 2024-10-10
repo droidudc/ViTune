2dd5466aaa1e9382719f60cecf88875430ca515f
droidudc <abhiramv670@gmail.com>
2024-10-10 21:00:27 +0530

    Rollback to old UI

---
+++ app/src/main/kotlin/app/vitune/android/ui/screens/home/HomeScreen.kt
@@ -0,0 +1,140 @@
+package app.vitune.android.ui.screens.home
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveableStateHolder
+import androidx.compose.ui.res.stringResource
+import app.vitune.android.Database
+import app.vitune.android.R
+import app.vitune.android.handleUrl
+import app.vitune.android.models.SearchQuery
+import app.vitune.android.models.toUiMood
+import app.vitune.android.preferences.DataPreferences
+import app.vitune.android.preferences.UIStatePreferences
+import app.vitune.android.query
+import app.vitune.android.ui.components.themed.Scaffold
+import app.vitune.android.ui.screens.GlobalRoutes
+import app.vitune.android.ui.screens.Route
+import app.vitune.android.ui.screens.albumRoute
+import app.vitune.android.ui.screens.artistRoute
+import app.vitune.android.ui.screens.builtInPlaylistRoute
+import app.vitune.android.ui.screens.builtinplaylist.BuiltInPlaylistScreen
+import app.vitune.android.ui.screens.localPlaylistRoute
+import app.vitune.android.ui.screens.localplaylist.LocalPlaylistScreen
+import app.vitune.android.ui.screens.mood.MoodScreen
+import app.vitune.android.ui.screens.mood.MoreAlbumsScreen
+import app.vitune.android.ui.screens.mood.MoreMoodsScreen
+import app.vitune.android.ui.screens.moodRoute
+import app.vitune.android.ui.screens.pipedPlaylistRoute
+import app.vitune.android.ui.screens.playlistRoute
+import app.vitune.android.ui.screens.search.SearchScreen
+import app.vitune.android.ui.screens.searchResultRoute
+import app.vitune.android.ui.screens.searchRoute
+import app.vitune.android.ui.screens.settingsRoute
+import app.vitune.android.utils.toast
+import app.vitune.compose.persist.PersistMapCleanup
+import app.vitune.compose.routing.Route0
+import app.vitune.compose.routing.RouteHandler
+
+private val moreMoodsRoute = Route0("moreMoodsRoute")
+private val moreAlbumsRoute = Route0("moreAlbumsRoute")
+
+@Route
+@Composable
+fun HomeScreen() {
+    val saveableStateHolder = rememberSaveableStateHolder()
+
+    PersistMapCleanup("home/")
+
+    RouteHandler {
+        GlobalRoutes()
+
+        localPlaylistRoute { playlistId ->
+            LocalPlaylistScreen(playlistId = playlistId)
+        }
+
+        builtInPlaylistRoute { builtInPlaylist ->
+            BuiltInPlaylistScreen(builtInPlaylist = builtInPlaylist)
+        }
+
+        moodRoute { mood ->
+            MoodScreen(mood = mood)
+        }
+
+        moreMoodsRoute {
+            MoreMoodsScreen()
+        }
+
+        moreAlbumsRoute {
+            MoreAlbumsScreen()
+        }
+
+        Content {
+            Scaffold(
+                topIconButtonId = R.drawable.settings,
+                onTopIconButtonClick = { settingsRoute() },
+                tabIndex = UIStatePreferences.homeScreenTabIndex,
+                onTabChange = { UIStatePreferences.homeScreenTabIndex = it },
+                tabColumnContent = { item ->
+                    item(0, stringResource(R.string.quick_picks), R.drawable.sparkles)
+                    item(1, stringResource(R.string.discover), R.drawable.globe)
+                    item(2, stringResource(R.string.songs), R.drawable.musical_notes)
+                    item(3, stringResource(R.string.playlists), R.drawable.playlist)
+                    item(4, stringResource(R.string.local), R.drawable.download)
+                }
+            ) { currentTabIndex ->
+                saveableStateHolder.SaveableStateProvider(key = currentTabIndex) {
+                    val onSearchClick = { searchRoute("") }
+                    when (currentTabIndex) {
+                        0 -> QuickPicks(
+                            onAlbumClick = { albumRoute(it.key) },
+                            onArtistClick = { artistRoute(it.key) },
+                            onPlaylistClick = {
+                                playlistRoute(
+                                    p0 = it.key,
+                                    p1 = null,
+                                    p2 = null,
+                                    p3 = it.channel?.name == "YouTube Music"
+                                )
+                            },
+                            onSearchClick = onSearchClick
+                        )
+
+                        1 -> HomeDiscovery(
+                            onMoodClick = { mood -> moodRoute(mood.toUiMood()) },
+                            onNewReleaseAlbumClick = { albumRoute(it) },
+                            onSearchClick = onSearchClick,
+                            onMoreMoodsClick = { moreMoodsRoute() },
+                            onMoreAlbumsClick = { moreAlbumsRoute() },
+                            onPlaylistClick = { playlistRoute(it, null, null, true) }
+                        )
+
+                        2 -> HomeSongs(
+                            onSearchClick = onSearchClick
+                        )
+
+                        3 -> HomePlaylists(
+                            onBuiltInPlaylist = { builtInPlaylistRoute(it) },
+                            onPlaylistClick = { localPlaylistRoute(it.id) },
+                            onPipedPlaylistClick = { session, playlist ->
+                                pipedPlaylistRoute(
+                                    p0 = session.apiBaseUrl.toString(),
+                                    p1 = session.token,
+                                    p2 = playlist.id.toString()
+                                )
+                            },
+                            onSearchClick = onSearchClick
+                        )
+
+                        
+
+                   
+
+                        4 -> HomeLocalSongs(
+                            onSearchClick = onSearchClick
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
